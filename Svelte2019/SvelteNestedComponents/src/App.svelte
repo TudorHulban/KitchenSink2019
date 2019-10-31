404c89b57dbeb78db7fd1af66e1abf7024fe60e5
Tudor Hulban <prindex2010@gmail.com>
2019-10-31 18:09:59 +0200

    Form sending data if CORS enabled on backend

--- Svelte2019/SvelteNestedComponents/src/App.svelte
+++ Svelte2019/SvelteNestedComponents/src/App.svelte
@@ -1,14 +1,17 @@
 <script>
   import C1 from "./C1.svelte";
+  import Login from "./Login.svelte";
   export let name;
   export let thevars;
 </script>
 
 <style>
   h1 {
-    color: purple;
+    margin: 10px 20px 30px 20px;
+    font-size: 40px;
   }
 </style>
 
 <h1>Hi {name}!</h1>
+<Login />
 <C1 v1={thevars.v2} />
