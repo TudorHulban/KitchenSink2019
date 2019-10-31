247e7473560c6affb5c31203f5d2655cdf596b6a
Tudor Hulban <prindex2010@gmail.com>
2019-10-31 16:55:36 +0200

    Add files via upload

---
+++ Svelte2019/SvelteNestedComponents/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import C1 from "./C1.svelte";
+  export let name;
+  export let thevars;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hi {name}!</h1>
+<C1 v1={thevars.v2} />
